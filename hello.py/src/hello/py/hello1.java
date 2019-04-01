a468b389e008b9eb150c63fce4e025d2aaf4882a
josooyun <josooyeon11@naver.com>
2019-04-01 09:48:00 +0900

    added 'her world'

--- hello.py/src/hello/py/hello1.java
+++ hello.py/src/hello/py/hello1.java
@@ -4,6 +4,7 @@ public class hello1 {
 public static void main(String[] args) {
 	System.out.println("Hello world");
     System.out.println("Hello his world");
+    System.out.println("Hello her wolrd");
 }
 
 }
